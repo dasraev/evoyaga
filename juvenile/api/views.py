6fa0773a4de23e7b69366460302720085c89cbae
Asrayev Dilshod <dasraev@Asrayevs-MacBook-Air.local>
2024-08-04 16:32:46 +0500

    nothing but for testing

--- juvenile/api/views.py
+++ juvenile/api/views.py
@@ -1556,6 +1556,7 @@ class JuvenilesInfoByProphylacticInspectorListView(generics.RetrieveAPIView):
         pinfl = self.kwargs.get('pinfl')
         prophylactic_inspector = get_object_or_404(models.ProphylacticInspector,pinfl=pinfl)
         return prophylactic_inspector
+#print
 
 
 
