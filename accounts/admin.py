99cd7b8f6456f96e6a417be71c6e864608391e02
Asrayev Dilshod <dasraev@Asrayevs-MacBook-Air.local>
2024-08-04 16:46:04 +0500

    django django admin create user with password

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 
 from .models import CustomUser
 
+
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('login', 'first_name',)
     list_display_links = ('login', 'first_name',)
+    def save_model(self, request, obj, form, change):
+        if change and 'password' not in form.changed_data:
+            pass
+        else:
+            obj.set_password(obj.password)
+        obj.save()
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
